0749e22855694d002c3e23b03cc8ed60acb6ea3f
lzno10 <liuzhuonlp@gmail.com>
2015-05-26 13:40:25 +0800

    Update display.php

--- display.php
+++ display.php
@@ -1,7 +1,14 @@
 <?php 
+$options = getopt("f:");
 
 while($line = fgets(STDIN)){
     $line = trim($line);
+    if (isset($options["f"])){
+        $idx = (int)$options["f"];
+        #echo $idx."\n";
+        $arr = explode("\t", $line);
+        $line = $arr[$idx - 1]; 
+    }   
     $obj = json_decode($line, true);
     print_r($obj);
 }
